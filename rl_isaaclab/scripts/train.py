b7740ab12df199b6daa5eee4e98f739f8b401c59
forrest-linux <1399541701@qq.com>
2026-07-13 13:03:16 +0800

    fix(adaption): 修复蒸馏时的重力设置问题

--- rl_isaaclab/scripts/train.py
+++ rl_isaaclab/scripts/train.py
@@ -73,6 +73,7 @@ def main(env_cfg: DirectRLEnvCfg, agent_cfg: dict):
     agent_cfg["algorithm"]['minibatch_size'] = min([args_cli.num_envs * 8, 32768])
     if agent_cfg["algo"] == "ProprioAdapt":
         env_cfg.gravity_curriculum = False
+        env_cfg.sim.gravity = (0.0, 0.0, -9.81)
     config = ConfigWrapper(agent_cfg, env_cfg)
 
     # specify directory for logging experiments
